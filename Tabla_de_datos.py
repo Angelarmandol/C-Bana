2ebb56f2fbe09b610ee1e62de5b5ed69cf4276cb
Angel Lopez <angelarmandol@hotmail.com>
2017-10-17 22:58:55 -0700

    Update Tabla_de_datos.py

--- Tabla_de_datos.py
+++ Tabla_de_datos.py
@@ -4,11 +4,39 @@ try:
 except ImportError:  # Python 3
     from tkinter import *
     from tkinter.ttk import *
+from array import *
+
+
 
 variable = "xxxx"
- 
+arreglo = array('d', [0.23, 0.654, 0.8763, 0.89034, 0.236, 0.3451, 0.345, 0.1231, 0.3241, 0.7230])
+
+
+''' Array
+‘b’ -> Represents signed integer of size 1 byte
+‘B’ -> Represents unsigned integer of size 1 byte
+‘c’ -> Represents character of size 1 byte
+‘u’ -> Represents unicode character of size 2 bytes
+‘h’ -> Represents signed integer of size 2 bytes
+‘H’ -> Represents unsigned integer of size 2 bytes
+‘i’ -> Represents signed integer of size 2 bytes
+‘I’ -> Represents unsigned integer of size 2 bytes
+‘w’ -> Represents unicode character of size 4 bytes
+‘l’ -> Represents signed integer of size 4 bytes
+‘L’ -> Represents unsigned integer of size 4 bytes
+‘f’ -> Represents floating point of size 4 bytes
+‘d’ -> Represents floating point of size 8 bytes
+
+
+'''
+
 
 class App(Frame):
+
+
+
+
+
 
 
 
@@ -21,13 +49,14 @@ class App(Frame):
         parent.grid_columnconfigure(0, weight = 1)
 
     def CreateUI(self):
+
         tv = Treeview(self)
         tv['columns'] = ('starttime', 'endtime', 'status')
-        tv.heading("#0", text='Numeros Pseudoaleatorios', anchor='w')
+        tv.heading("#0", text='*////*', anchor='w')
         tv.column("#0", anchor="w")
         tv.heading('starttime', text='bbbb')
         tv.column('starttime', anchor='center', width=100)
-        tv.heading('endtime', text='lo que sea')
+        tv.heading('endtime', text='Numeros Pseudoaleatorios')
         tv.column('endtime', anchor='center', width=100)
         tv.heading('status', text='No se')
         tv.column('status', anchor='center', width=100)
@@ -37,13 +66,13 @@ class App(Frame):
         self.grid_columnconfigure(0, weight = 1)
 
 
-        for i in range(99999):
+        for i in range(10):
             self.treeview.insert('', '999', text=variable, values=('xXx',
-                             i, 'Ok'))
+                             arreglo[i], 'Ok'))
 
     def LoadTable(self):
-        self.treeview.insert('', '999', text=variable, values=('xXx',
-                             '0.0xxx', 'Ok'))
+         self.treeview.insert('', '999', text=variable, values=('xXx',
+                              '0.0xxx', 'Ok'))
     
 
 def main():
