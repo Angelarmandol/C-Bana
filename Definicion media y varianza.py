50d6901de3ff923490b514140a0b59167de7d4fa
futarius96 <32892257+futarius96@users.noreply.github.com>
2017-11-09 21:27:50 -0800

    Add files via upload

---
+++ Definicion media y varianza.py
@@ -0,0 +1,7 @@
+a = array('d',[4,6,3,6,7,2,5])
+def sacarMediayVar(a):  """saca la media y la varianza"""
+tamA=len(a) """numero de datos en el arreglo"""
+media = [(2*tamA)-1]/3 
+varianza= [(16*tamA)-29]/90
+print "la media es: "+media
+print "la varianza es: "+varianza
