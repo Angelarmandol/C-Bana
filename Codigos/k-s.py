f92445684bc0e9f56a3a775de5cddcc5230f7175
futarius96 <32892257+futarius96@users.noreply.github.com>
2017-11-15 20:42:30 -0800

    Add files via upload

---
+++ Codigos/k-s.py
@@ -0,0 +1,50 @@
+aOrd=[]
+funcion=[]
+derivada=[]
+Dmas=[]
+Dmenos[]
+exp=2.718281828
+def metodok-s(a):
+aOrd=a
+burbuja(aOrd)
+funcionM()
+fprima()
+calcularDmas(funcion, fprima)
+calcularDmenos(funcion,fprima)
+
+	
+def	burbuja(aOrd): #ordena el arreglo de menor a mayor
+	for i in range(0, len(aOrd)-1):
+		for j in range(0,len(aOrd)-1-i):
+			if aOrd[j] > aOrd[j+1]:
+				aOrd[j],aOrd[j+1]=aOrd[j+1],aOrd[j]
+return aOrd
+
+def funcionM: #paso 3 f(x)=i/n
+for x in xrange(0,len(aOrd)):
+	pass
+	funcion[x]=(x+1)/len(aOrd)
+	pass
+	return funcion
+
+def fprima():#f'(x)=1-e(-xi/1.22)
+	pass
+	for x in xrange(0,len(aOrd)):
+		derivada[x]=1-(exp*(aOrd/1.22))
+		pass
+return derivada
+
+def calcularDmas(funcion,fprima):#paso 5.1 D+=f(x)-f'(x)
+	pass
+	for x in xrange(0,len(aOrd):
+		Dmas[x]=funcion[x]-fprima[x]
+		pass
+return derivada
+
+
+def calcularDmenos(funcion, fprima): #paso 5.2 D-=F'(xi)-f(xi-1)
+for x in xrange(0,len(aOrd)):
+	pass
+	Dmenos= fprima[x]- funcion[x]
+	pass
+return Dmenos
