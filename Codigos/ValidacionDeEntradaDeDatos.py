dc5a16309d28803316964fb327a4daa64c400813
Angel Lopez <angelarmandol@hotmail.com>
2017-11-21 13:13:09 -0800

    Update ValidacionDeEntradaDeDatos.py

--- Codigos/ValidacionDeEntradaDeDatos.py
+++ Codigos/ValidacionDeEntradaDeDatos.py
@@ -1,20 +1,23 @@
-##La cadena de texto se guarda en a, el vector se regresa como a.
-
-'''
-Validacion de los datos:
-__________________
-Ya hechas:
-1) Todos numericos (con puntuacion)
-2) Sin puntos dobles
-__________________
-Por hacer
-'''
-
+ 
 import random
+'''
+Criterios:
+Es numerico: numerico
+Son mas o igual a 50: mayorcincuenta
+Algun numnero valido ( por defecto, si): numervalido
+
+pasa o no pasa: pasa
+'''
+
+numerico=0
+mayorcincuenta=0
+numerovalido=1
+
+pasa=0
 #a = "0.25051451 0.8521 0.5454 0.54545 0.545454 0.65665 0.14564654 0.156654156 0.5458454 0.5445445 0.787878 0.986535"
 a = " "
 alea=0
-for i in range(20):
+for i in range(50):
 	alea = random.random()
 	a = a+" "+repr(alea)
 
@@ -23,9 +26,10 @@ vector = []
 
 if(a.isdigit()):
 	print ("no es numerica")
-
+	numerico=0
 else:
 	print("numerica")
+	numerico=1
 	print (" ")
 	a = a.split(" ")
 	a.pop(0)
@@ -36,12 +40,26 @@ else:
 
 rango = len(a)
 
+if(rango>=50):
+	print ("Son mas de cincuenta, si pasa")
+	mayorcincuenta=1
+else:
+	print("Son menos de cincuanta, no pasa")
+	mayorcincuenta=0
 try:
 	for i in range (rango):
 		float(a[i])
 except ValueError:
     print "Oops!  "+repr(a[i])+"no es un numero valido"
+    numerovalido=0
 
-
+if(numerico == 1 and mayorcincuenta == 1 and numerovalido==1):
+	print("No hay nada malo")
+else:
+	print("si hay algo malo")
+	numerico=0
+	mayorcincuenta=0
+	numerovalido=1
+	pasa=0
 
  
