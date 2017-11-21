b4ffccafde14d528ae9f73dfa3543f5c0cb99eb3
Angel Lopez <angelarmandol@hotmail.com>
2017-11-21 13:02:34 -0800

    Update ValidacionDeEntradaDeDatos.py

--- Codigos/ValidacionDeEntradaDeDatos.py
+++ Codigos/ValidacionDeEntradaDeDatos.py
@@ -1,24 +1,22 @@
-
 ##La cadena de texto se guarda en a, el vector se regresa como a.
 
 '''
-
 Validacion de los datos:
 __________________
 Ya hechas:
 1) Todos numericos (con puntuacion)
 2) Sin puntos dobles
-
 __________________
 Por hacer
-
-
-
-
 '''
 
-a = "0.25051451 0.8521 0.5454 0.54545 0.545454 0.65665 0.14564654 0.156654156 0.5458454 0.5445445 0.787878 0.986535"
-
+import random
+#a = "0.25051451 0.8521 0.5454 0.54545 0.545454 0.65665 0.14564654 0.156654156 0.5458454 0.5445445 0.787878 0.986535"
+a = " "
+alea=0
+for i in range(20):
+	alea = random.random()
+	a = a+" "+repr(alea)
 
 vector = []
 
@@ -30,6 +28,8 @@ else:
 	print("numerica")
 	print (" ")
 	a = a.split(" ")
+	a.pop(0)
+	a.pop(0)
 	b = len(a)
 	for i in range (b):
 		print (a[i])
@@ -41,3 +41,7 @@ try:
 		float(a[i])
 except ValueError:
     print "Oops!  "+repr(a[i])+"no es un numero valido"
+
+
+
+ 
