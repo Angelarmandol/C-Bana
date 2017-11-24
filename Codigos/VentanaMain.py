52a35598442d1ff5f12c1a6aafb8b83b8aa4f5fa
Angel Lopez <angelarmandol@hotmail.com>
2017-11-24 10:29:33 -0800

    Update VentanaMain.py

--- Codigos/VentanaMain.py
+++ Codigos/VentanaMain.py
@@ -1,6 +1,6 @@
 try:
-	from tkinter import *
-	from tkinter.ttk import *
+    from tkinter import *
+    from tkinter.ttk import *
 
     
 except ImportError:  # Python 3
@@ -25,135 +25,166 @@ pasa o no pasa: pasa
 '''
 
 
-variable = "xxxx"
 arreglo = array('d', [0.23, 0.654, 0.8763, 0.89034, 0.236, 0.3451, 0.345, 0.1231, 0.3241, 0.7230])
 ventana = tk.Tk()
 ventana.geometry("800x600")
 ventana.title("C-Bana")
 vector = []
 a = []
+rango2=0
 
 def resolucionPrincipal(resol):
-	ventana.geometry("1000x600")
-	#ventana.mainloop()
+    ventana.geometry("1000x600")
+    #ventana.mainloop()
 
 def hola():
     print ("Hola!")
 
 
-rango=0
-def comprobar(a):
-	 
-	 
-	a=texto.get("1.0","end-1c")
-	numerico=0
-	mayorcincuenta=0
-	numerovalido=1
 
-	pasa=0
-	#a = "0.25051451 0.8521 0.5454 0.54545 0.545454 0.65665 0.14564654 0.156654156 0.5458454 0.5445445 0.787878 0.986535"
-	'''
-	a = " "
-	alea=0
-	for i in range(50):
-		alea = random.random()
-		a = a+" "+repr(alea)
+def comprobar():
+    global a
+     
+
+
+
+
+    a=texto.get("1.0","end-1c")
+    numerico=0
+    mayorcincuenta=0
+    numerovalido=1
+
+    pasa=0
+    #a = "0.25051451 0.8521 0.5454 0.54545 0.545454 0.65665 0.14564654 0.156654156 0.5458454 0.5445445 0.787878 0.986535"
+    '''
+    a = " "
+    alea=0
+    for i in range(50):
+        alea = random.random()
+        a = a+" "+repr(alea)
 '''
-	
+    
 
-	#print (a)
-	if(a.isdigit()):
-		print ("no es numerica")
-		numerico=0
-	else:
-		print("numerica")
-		numerico=1
-		print (" ")
-		#print (a)
-		
+    #print (a)
+    if(a.isdigit()):
+        print ("no es numerica")
+        numerico=0
+    else:
+        print("numerica")
+        numerico=1
+        print (" ")
+        #print (a)
+        
 
-		a = a.split(" ")
-		#print(a)
-		#a.pop(0)
-		#a.pop(0)
-		b = len(a)
-		contador=0
-		
-	'''
-		for i in range (b):
-			print (a[i])
-			print("/////"+repr(b))
-	'''
-	a=a[0].split("\n")
-	print(a[0])
-	print(a[1])
-	print ("longiyud es "+repr(len(a)))
-	rango = len(a)
-	
-	if(rango>=50):
-		print ("Son mas de cincuenta, si pasa")
-		mayorcincuenta=1
-	else:
-		print("Son menos de cincuanta, no pasa")
-
-		mayorcincuenta=0
-	'''
-	try:
-		for i in range (rango):
-			float(a[i])
-	except ValueError:
-	    print ("Oops!  "+repr(a[i])+"no es un numero valido")
-	    #numerovalido=0
-	'''
-
-	if(numerico == 1 and mayorcincuenta == 1 and numerovalido==1):
-		print("No hay nada malo")
-		return a
-	else:
-		print("si hay algo malo")
-		numerico=0
-		mayorcincuenta=0
-		numerovalido=1
-		pasa=0
+        a = a.split(" ")
+        #print(a)
+        #a.pop(0)
+        #a.pop(0)
+        b = len(a)
+        contador=0
+        
+    '''
+        for i in range (b):
+            print (a[i])
+            print("/////"+repr(b))
+    '''
+    a=a[0].split("\n")
+    print(a[0])
+    print(a[1])
+    print ("longiyud es "+repr(len(a)))
+    rango = len(a)
+    
 
 
-	
+
+
+
+
+    global rango2
+    rango2=rango
+    print("se a guardado como rango2 "+repr(rango2))
+  
+
+
+
+
+
+
+
+    if(rango>=50):
+        print ("Son mas de cincuenta, si pasa")
+        mayorcincuenta=1
+    else:
+        print("Son menos de cincuanta, no pasa")
+
+        mayorcincuenta=0
+        ctypes.windll.user32.MessageBoxW(0, "No hay numeros suficientes para generar la tabla", "Error", 1)
+    '''
+    try:
+        for i in range (rango):
+            float(a[i])
+    except ValueError:
+        print ("Oops!  "+repr(a[i])+"no es un numero valido")
+        #numerovalido=0
+    '''
+
+    if(numerico == 1 and mayorcincuenta == 1 and numerovalido==1):
+        print("No hay nada malo")
+       
+    else:
+        print("si hay algo malo")
+        numerico=0
+        mayorcincuenta=0
+        numerovalido=1
+        pasa=0
+
+    print("Termina comprobar, a es:")
+    print (a[0])
+    print (a[1])
+
+    
 def otraventana():
-	
-	print ("cantidad esperada es "+repr(len(a)))
-	comprobar(a)
-	if (len(a)!=0):
-		ventana2 = tk.Tk()
-		tv = Treeview(ventana2)
-		
-		ventana2.geometry("600x200")
-		ventana2.resizable(False, False) #para no permitir 
-		ventana2.title("Tabla de datos")
-		tv['columns'] = ('starttime', 'endtime', 'status')
-		tv.heading("#0", text='*////*', anchor='w')
-		tv.column("#0", anchor="w")
-		tv.heading('starttime', text='bbbb')
-		tv.column('starttime', anchor='center', width=100)
-		tv.heading('endtime', text='Numeros Pseudoaleatorios')
-		tv.column('endtime', anchor='center', width=100)
-		tv.heading('status', text='No se')
-		tv.column('status', anchor='center', width=100)
-		tv.grid(sticky = (N,S,W,E))
-		
-		
-		print("longitud de a es "+repr(b))
-		#insert normal
-		#tv.insert('', '999', text=variable, values=('xXx','99', 'Ok'))
-		for i in range(b):
-	            tv.insert('', '999', text=variable, values=('xXx',a[i], 'Ok'))
-		#self.treeview = tv
-		#self.grid_rowconfigure(0, weight = 1)
-		#self.grid_columnconfigure(0, weight = 1)
-		ventana2.mainloop()
-	else:
-	 print("no hay datos")
-	 b = 0
-	 ctypes.windll.user32.MessageBoxW(0, "No hay numeros suficientes para generar la tabla", "Error", 1)
+    
+    comprobar()
+    print("el valor de la variable rango 2 es: "+repr(rango2))
+    ventana2 = tk.Tk()
+    tv = Treeview(ventana2)
+        
+    ventana2.geometry("600x200")
+    ventana2.resizable(False, False) #para no permitir 
+    ventana2.title("Tabla de datos")
+    tv['columns'] = ('starttime', 'endtime', 'status', 'wea1')
+
+ 
+
+    tv.heading('starttime', text='Numeros Ordenados')
+    tv.column('starttime', anchor='center', width=100)
+
+    tv.heading('endtime', text='Numeros Pseudoaleatorios')
+    tv.column('endtime', anchor='center', width=100)
+
+    tv.heading('status', text='f(x)=i/n')
+    tv.column('status', anchor='center', width=100)
+
+    tv.heading('wea1', text='wea2')
+    tv.column('wea1', anchor='center', width=100)
+
+    tv.grid(sticky = (N,S,W,E))
+        
+        
+    print("longitud de a es "+repr(rango2))
+    #insert normal
+    #tv.insert('', '999', text=variable, values=('xXx','99', 'Ok'))
+
+    print("valor a list: "+repr(a[0]))
+    for g in range(rango2):
+        tv.insert('', 'end', values=('burbuja',a[g], 'EfeDeEquisIEntreEne', 'wea3'))
+    #self.treeview = tv
+    #self.grid_rowconfigure(0, weight = 1)
+    #self.grid_columnconfigure(0, weight = 1)
+    ventana2.mainloop()
+        
+    
 ##  Styles:
 ##  0 : OK
 ##  1 : OK | Cancel
@@ -163,7 +194,7 @@ def otraventana():
 ##  5 : Retry | No 
 ##  6 : Cancel | Try Again | Continue
 
-	 
+     
     
 
 
@@ -216,7 +247,7 @@ texto = Text(ventana, height=20, width=40)
 texto.pack()
 texto.config(width=200)
  
-boton = tk.Button(ventana, text="Comprobar", command=comprobar)	
+boton = tk.Button(ventana, text="Comprobar", command=comprobar) 
 boton.pack()
 
 #Muestra la ventana
